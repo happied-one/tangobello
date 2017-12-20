7b089e9a00148e8156a181e83b1fc114044a8f77
hsp <1615433864@qq.com>
2017-12-20 09:09:11 +0800

    view cover when write articles.

--- run.py
+++
@@ -1,9 +0,0 @@
-from bottle import run
-from tangobello.app import init_app
-
-
-app = init_app()
-
-
-if __name__ == '__main__':
-    run(app, debug=True, reloader=True, port=1110)
--- website/static/js/main.js
+++
@@ -1,9 +0,0 @@
-function showSearchForm() {
-    var search_form = document.getElementById("search-form");
-    var display_val = search_form.style.display;
-    if (!display_val || display_val == "none"){
-        search_form.style.display = "block";
-    }else{
-        search_form.style.display = "none";
-    }
-}
